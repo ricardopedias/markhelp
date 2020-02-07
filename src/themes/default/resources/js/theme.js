8486a074f85396969fc5fe735e6288ecb0743a7f
Ricardo Pereira <contato@ricardopdias.com.br>
2020-02-07 20:56:50 -0300

    Diversas otimizações no código

--- src/themes/default/resources/js/theme.js
+++ src/themes/default/resources/js/theme.js
@@ -3,16 +3,8 @@
 Este script contém a estrutura do javascript a ser compilado para o tema.
 ---------------------------------------------------------------------------------------------
 */
-try {
-
-    window.$ = window.jQuery = require("jquery");
-
-} catch (e) {
-    console.log(e);
-}
-
+window.$ = window.jQuery = require("jquery");
 require("html5shiv");
 require("./sidebar");
-
 const hljs = require("highlight.js");
 hljs.initHighlightingOnLoad();

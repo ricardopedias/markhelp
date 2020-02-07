210e5ff95c6b8001d0a098456ffc7667756cc5d4
Ricardo Pereira <contato@ricardopdias.com.br>
2020-02-07 20:39:55 -0300

    Diversas otimizações no código

--- src/Handle.php
+++ src/Handle.php
@@ -51,4 +51,58 @@ abstract class Handle
     {
         return $this->structure;
     }
+
+    // Funções seguras
+
+    public function include($filename)
+    {
+        $filename = filter_var($filename, FILTER_SANITIZE_STRING);
+        return include($filename);
+    }
+
+    public function getContents($filename)
+    {
+        $filename = filter_var($filename, FILTER_SANITIZE_STRING);
+        return file_get_contents($filename);
+    }
+
+    public function isFile($filename)
+    {
+        $filename = filter_var($filename, FILTER_SANITIZE_STRING);
+        return is_file($filename);
+    }
+
+    public function isDirectory($path)
+    {
+        $path = filter_var($path, FILTER_SANITIZE_STRING);
+        return is_dir($path);
+    }
+
+    public function dirname($path, $levels = 1)
+    {
+        $path = filter_var($path, FILTER_SANITIZE_STRING);
+
+        for($x=0; $x < $levels; $x++) {
+            $path = dirname($path);
+        }
+        return $path;
+    }
+
+    public function basename($filename)
+    {
+        return preg_replace('/^.+[\\\\\\/]/', '', $filename);
+    }
+
+    public function filename($filename)
+    {
+        $basename = self::basename($filename);
+        return preg_replace('/\\.[^.\\s]{3,4}$/', '', $basename);
+    }
+
+    public function copy($filename, $newFilename)
+    {
+        $filename = filter_var($filename, FILTER_SANITIZE_STRING);
+        $newFilename = filter_var($newFilename, FILTER_SANITIZE_STRING);
+        return copy($filename, $newFilename);
+    }
 }

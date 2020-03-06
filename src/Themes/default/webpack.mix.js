715efe387f01dda1f06889081ec9636971babdc5
Ricardo Pereira <contato@ricardopdias.com.br>
2020-03-06 11:02:23 -0300

    Diversas melhoria no tema padrão

--- src/Themes/default/webpack.mix.js
+++ src/Themes/default/webpack.mix.js
@@ -4,12 +4,12 @@ Este arquivo é responsável pela compilação dos scripts sass e javascript
 usando laravel mix. Ex: 
     $ npm run dev 
     ou 
-    $npm run prod
+    $ npm run prod
 ---------------------------------------------------------------------------------------------
 */
 
 let mix = require("laravel-mix");
 
 mix.disableNotifications();
-mix.js("src/themes/default/resources/js/theme.js', 'src/themes/default/assets/scripts.js");
-mix.sass("src/themes/default/resources/scss/theme.scss', 'src/themes/default/assets/styles.css");
+mix.js("resources/js/theme.js", "assets/scripts.js");
+mix.sass("resources/scss/theme.scss", "assets/styles.css");

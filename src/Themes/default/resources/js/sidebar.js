2641afd8396fff9e25e8b35de36ed3d2633b6049
Ricardo Pereira <contato@ricardopedias.com.br>
2020-09-15 11:30:48 -0300

    Otimizações no tema padrão

--- src/Themes/default/resources/js/sidebar.js
+++ src/Themes/default/resources/js/sidebar.js
@@ -1,30 +1,52 @@
+import Choices from 'choices.js';
 
-let elements = document.querySelectorAll('select.version-select');
+document.querySelectorAll('select.version-select').forEach(function(select, key){
 
-elements.forEach(function(select, key){
+    new Choices(select, {
+        searchEnabled: false,
+        loadingText: 'Carregando...',
+        noResultsText: 'Sem resultados',
+        noChoicesText: 'Nada para selecionar',
+        itemSelectText: 'Selecione',
+    });
+
     select.addEventListener('change', function(){
         let versionUrl = this.options[this.selectedIndex].value;
         window.location.href = versionUrl;
     });
 });
 
+document.querySelectorAll('menu ul li').forEach(function(element, key){
 
-// $("menu ul li:has(ul)").each(function(){
+    let sublist = element.querySelector('ul');
+    if (sublist !== null) {
 
-//     $(this).addClass("has-childs");
+        let item = element.querySelector('a');
+        item.setAttribute('href', 'javascript:void(0);');
 
-//     $(this).find("> a")
-//         .attr({ href: "javascript:void(0);" })
-//         .click(function() {
-            
-//             if ($(this).parent().hasClass("open")) {
-//                 $(this).parent().removeClass("open");
-//                 $(this).find(".arrow").removeClass("up").addClass("down");
-//                 return;
-//             }
+        // item.insertAdjacentHTML('beforeend', '<i class="arrow down"></i>');
 
-//             $(this).parent().addClass("open");
-//             $(this).find(".arrow").removeClass("down").addClass("up");
-//         })
-//         .append("<i class='arrow down'></i>");
-// });
+        item.addEventListener('click', function (event) {
+
+            let parent = event.target.parentNode;
+    
+            if (parent.classList.contains('open') === true) {
+                parent.classList.remove('open');
+                let arrow = event.target.querySelector('.arrow');
+                arrow.classList.remove('up');
+                arrow.classList.remove('down')
+                arrow.classList.add('down');
+                return;
+            }
+
+            parent.classList.add('open');
+            let arrow = event.target.querySelector('.arrow');
+                arrow.classList.remove('up');
+                arrow.classList.remove('down')
+                arrow.classList.add('up');
+
+            // Não segue o link
+            event.preventDefault();
+        }, false);
+    }
+});
